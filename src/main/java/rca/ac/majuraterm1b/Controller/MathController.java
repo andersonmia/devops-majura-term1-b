8902ba92bccd31bc31988cf136b63b15f8da4a01
Majura Anthony Gaudence <andersonmia1968@gmail.com>
2023-12-18 10:29:28 +0200

    controller

---
+++ src/main/java/rca/ac/majuraterm1b/Controller/MathController.java
@@ -0,0 +1,22 @@
+package rca.ac.majuraterm1b.Controller;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rca.ac.majuraterm1b.DTO.DoMathReqDTO;
+import rca.ac.majuraterm1b.Exceptions.InvalidOperationException;
+import rca.ac.majuraterm1b.Service.ServiceImpl.MathOperatorImpl;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/v1/doMath")
+public class MathController {
+    private final MathOperatorImpl mathOperatorImpl;
+
+    public ResponseEntity<?> doMath(@RequestBody DoMathReqDTO doMathReqDTO) throws InvalidOperationException {
+        return ResponseEntity.ok(mathOperatorImpl.doMath(doMathReqDTO.getOperand1(), doMathReqDTO.getOperand2(), doMathReqDTO.getOperation()));
+    }
+}
