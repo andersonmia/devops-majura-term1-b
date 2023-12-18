e1476a6a5c64e9a01f268f01a590da4f0848e514
Majura Anthony Gaudence <andersonmia1968@gmail.com>
2023-12-18 09:11:05 +0200

    first commit

---
+++ src/main/java/rca/ac/majuraterm1b/MajuraTerm1BApplication.java
@@ -0,0 +1,13 @@
+package rca.ac.majuraterm1b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajuraTerm1BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MajuraTerm1BApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rca/ac/majuraterm1b/MajuraTerm1BApplicationTests.java
@@ -0,0 +1,13 @@
+package rca.ac.majuraterm1b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajuraTerm1BApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
